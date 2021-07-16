b6423c095af5eb9bfd69c42c9bff45eaf2f795cc
Rob <robert.gentile@powerhrg.com>
2021-07-16 14:15:32 -0400

    Completed Assignment

--- src/Components/TVShow.js
+++ src/Components/TVShow.js
@@ -1,10 +1,13 @@
 import React from "react";
 
-function TVShow({props}) {
+function TVShow(props) {
   return (
     <div>
       <br />
-      <img src={props.show.image.medium} onClick={props.selectShow} alt="" />
+      <img src={props.show.image.medium} 
+      onClick={() => props.selectShow(props.show)} 
+      alt=""
+      />
     </div>
   );
 }
